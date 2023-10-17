7ec9b6a45ca38680c342504780f4dbfde9f3f96d
Naman <namankumar62039@gmail.com>
2023-10-18 00:28:45 +0530

    bulding search bar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { toggleMenu } from '../utils/ReduxSlice';
+import {YOUTUBE_SEARCH_API} from "../utils/Constants"
 
 const Header = () => {
 
-  const Dispatch = useDispatch();
   const [searchQuery, setSearchQuery] = useState("");
+  const Dispatch = useDispatch();
+
+  useEffect(() => {
+    // console.log(searchQuery)
+  const timer = setTimeout(() =>  getSearchSegetion(),200)
+
+  return () => {
+    clearTimeout(timer)
+  };
+
+  },[searchQuery])
 
   const toggleMenuHandeler = () => {
     Dispatch(toggleMenu());
   };
 
+  const getSearchSegetion = async () => {
+    const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await data.json();
+
+    console.log(json[1])
+  } 
 
 
   return (
-    <div className='grid grid-flow-col m-2 p-5 shadow-lg'>
-      <div className='flex col-span-1'>
+    <div className='grid grid-flow-col p-5 shadow-lg w-full fixed bg-white'>
+      <div className='flex col-span-1 '>
         <svg className='h-6 mr-5 cursor-pointer' onClick={toggleMenuHandeler} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
         <img className='h-7' alt=' youTubelogo' src='https://image.shutterstock.com/image-photo/image-260nw-2310134969.jpg' />
       </div>
 
       <div className='h-8 col-span-10'>
-        <input placeholder='Search' type='text' className='border border-gray-400 w-6/12 rounded-l-full px-4 py-1'/>
+        <div>
+        <input placeholder='Search' type='text' value={searchQuery} className='border border-gray-400 w-6/12 rounded-l-full px-4 py-1' onChange={(e) => setSearchQuery(e.target.value)} />
         <button className='border border-gray-400 px-5 py-1 rounded-r-full bg-slate-200'>🔍</button>
+        </div>
+        <div className='fixed bg-white py-2 px-5 w-96'>
+          <ul>
+            <li>baby</li>
+            <li>baby corn</li>
+            <li>baby soup</li>
+            <li>baby shark</li>
+            <li>baby movie</li>
+          </ul>
+        </div>
       </div>
 
       <div className='h-8 col-span-1'>
