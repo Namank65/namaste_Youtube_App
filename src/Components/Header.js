b00c6693886449dfbc51cc9ca3c903904c554f95
Naman <namankumar62039@gmail.com>
2023-10-19 01:10:00 +0530

    bulding searchbaar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,11 +6,13 @@ import {YOUTUBE_SEARCH_API} from "../utils/Constants"
 const Header = () => {
 
   const [searchQuery, setSearchQuery] = useState("");
+  const [suggestions, setSeggestions] = useState([]);
+  const [showSuggestions, setShowSeggestions] = useState(true);
   const Dispatch = useDispatch();
 
-  useEffect(() => {
-    // console.log(searchQuery)
+
   const timer = setTimeout(() =>  getSearchSegetion(),200)
+  useEffect(() => {
 
   return () => {
     clearTimeout(timer)
@@ -25,13 +27,13 @@ const Header = () => {
   const getSearchSegetion = async () => {
     const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await data.json();
-
-    console.log(json[1])
-  } 
+    // console.log(json)
+    // setSeggestions(json[1]);
+  };
 
 
   return (
-    <div className='grid grid-flow-col p-5 shadow-lg w-full fixed bg-white'>
+    <div className='grid grid-flow-col p-5 shadow-lg bg-white'>
       <div className='flex col-span-1 '>
         <svg className='h-6 mr-5 cursor-pointer' onClick={toggleMenuHandeler} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>
         <img className='h-7' alt=' youTubelogo' src='https://image.shutterstock.com/image-photo/image-260nw-2310134969.jpg' />
@@ -42,19 +44,16 @@ const Header = () => {
         <input placeholder='Search' type='text' value={searchQuery} className='border border-gray-400 w-6/12 rounded-l-full px-4 py-1' onChange={(e) => setSearchQuery(e.target.value)} />
         <button className='border border-gray-400 px-5 py-1 rounded-r-full bg-slate-200'>🔍</button>
         </div>
+
         <div className='fixed bg-white py-2 px-5 w-96'>
           <ul>
-            <li>baby</li>
-            <li>baby corn</li>
-            <li>baby soup</li>
-            <li>baby shark</li>
-            <li>baby movie</li>
+            {/* {suggestions.map((sugg) => <li>🔍 {sugg}</li> )} */}
           </ul>
         </div>
       </div>
 
       <div className='h-8 col-span-1'>
-        <svg className='' xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" height="25px" viewBox="0 0 448 512"><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" /></svg>
       </div>
     </div>
   )
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -8,7 +8,7 @@ const Sidebar = () => {
   if(!isMenuOpen) return null;
   
   return (
-    <div className='p-5 text-left w-48 shadow-lg bg-slate-200 '>
+    <div className='p-5 text-left w-48 shadow-lg bg-slate-200'>
       <ul className='border-b border-gray-500 pb-2'>
       <Link to={'/'}><li>Home</li></Link>
         <li>Shorts</li>
