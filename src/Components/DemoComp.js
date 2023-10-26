3158234cd2a8b2a3362829a87323354dc99c87cb
Naman <namankumar62039@gmail.com>
2023-10-27 00:58:31 +0530

    learned useMemo and useRef

--- src/Components/DemoComp.js
+++ src/Components/DemoComp.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react';
+import {findPrime} from '../utils/Helper';
 
 const DemoComp = () => {
 
-    const [text, setText] = useState("");
+    const [text, setText] = useState(0);
+    const [isDark, setIsDark] = useState(false);
+
+    const prime = useMemo(() => findPrime(text), [text]);
+
+    console.log("Is rendering...")
 
 
   return (
-    <div className='h-96 w-96 m-5 p-5 border border-black'>
+    <div className={"h-96 w-96 m-5 p-5 border border-black" + (isDark && "bg-black text-red-500")}>
         <h1>Prime Number Generator</h1>
         <input className='border border-black p-2 m-3' type='number' value={text} onChange={(e) => setText(e.target.value)} placeholder='Enter Number'/>
-        <span>{text}</span>
+        <span>{prime}</span>
         <button className='border border-black p-1 m-1 bg-green-300'>Calculate</button>
+        <button className='border border-black p-1 m-1 bg-red-300' onClick={() => setIsDark(!isDark)}>Toggle</button>
     </div>
   )
 };
